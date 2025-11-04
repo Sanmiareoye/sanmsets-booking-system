3e559233ac73a9f304505c979ba5a71d57caf30e
Sanmi Areoye <sanmiareoye1@gmail.com>
2025-11-04 18:52:22 +0000

    slots

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -45,8 +45,6 @@ const availableDates = [
   "2025-11-10", // Monday
   "2025-11-11", // Tuesday
   "2025-11-12", // Wednesday
-  "2025-11-13", // Thursday
-  "2025-11-14", // Friday
   "2025-11-15", // Saturday
   "2025-11-17", // Monday
   "2025-11-18", // Tuesday
